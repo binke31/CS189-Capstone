acf1da84386357b1360e0f79ecebbbd3b1992af8
Chris Horuk <chris@horukmail.com>
2013-04-12 15:19:31 -0700

    just a few lines of code to add the current time to the paymentDate of a rental payment. although I left it commented out because I am not sure if we actually want to do this...

--- easy_rent_devise/app/controllers/rent_payment_controller.rb
+++ easy_rent_devise/app/controllers/rent_payment_controller.rb
@@ -11,6 +11,9 @@ class RentPaymentController < ApplicationController
   def create
 	  # params[:rentPayment] should have all the form input
 	  @rentPayment = RentPayment.new(params[:rentPayment])
+      time = Time.now
+      date = params[:rentPayment][:paymentDate].split("/")
+      @rentPayment.paymentDate = "20#{date[2]}/#{date[0]}/#{date[1]} #{time.hour}:#{time.min}:#{time.sec} #{time.zone}".to_datetime
 	  @rentPayment.save
 	  redirect_to "/home/", notice: "Rent payment successfully submitted!"
   end
