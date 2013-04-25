999ebbe2f64526ad31d898665680797f5b756371
Chris Horuk <chris@horukmail.com>
2013-04-25 10:14:28 -0700

    fixed the edit account settings stuff

--- easy_rent_devise/app/controllers/user_registrations_controller.rb
+++ easy_rent_devise/app/controllers/user_registrations_controller.rb
@@ -3,7 +3,39 @@ class UserRegistrationsController < Devise::RegistrationsController
   layout 'settingsPage'
     
   def edit
-    super
+    @user = current_user
+  end
+
+  def update_user
+    @user = User.find(current_user.id)
+    if params[:user][:password] != "" && params[:user][:email] != ""
+      if @user.update_attributes(params[:user])
+        # Sign in the user by passing validation in case his password changed
+        sign_in @user, :bypass => true
+        redirect_to user_root_path, notice: "Account Settings Updated Successfully!"
+      else
+        render "edit"
+      end
+    elsif params[:user][:password] == "" && params[:user][:email] != ""
+      if @user.update_attributes(email: params[:user][:email])
+        # Sign in the user by passing validation in case his password changed
+        sign_in @user, :bypass => true
+        redirect_to user_root_path, notice: "Account Settings Updated Successfully!"
+      else
+        render "edit"
+      end
+    elsif params[:user][:password] != "" && params[:user][:email] == ""
+      if @user.update_attributes(password: params[:user][:password], password_confirmation: params[:user][:password_confirmation])
+        # Sign in the user by passing validation in case his password changed
+        sign_in @user, :bypass => true
+        redirect_to user_root_path, notice: "Account Settings Updated Successfully!"
+      else
+        render "edit"
+      end
+    else
+      flash[:error] = "You can't submit a blank form!"
+      render :edit
+    end
   end
   
 end
