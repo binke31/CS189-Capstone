65627bf7d9abca0ab5f9665547fca05e467c034e
choruk <chris@horukmail.com>
2013-05-15 14:49:37 -0700

    added text messaging into the maintenance notification code

--- easy_rent_devise/app/models/maintenance_request.rb
+++ easy_rent_devise/app/models/maintenance_request.rb
@@ -12,7 +12,34 @@ class MaintenanceRequest < ActiveRecord::Base
   validates :writtenRequest, length: { maximum: 1000 }
   
   def sendMaintenanceNotification
-    NotificationMailer.maintenanceNotification(user.email, id).deliver
+    if user.notification_settings.maintenanceUpdates == "on"
+      
+      if user.notification_settings.emails == "on"
+        NotificationMailer.maintenanceNotification(user.email, id).deliver
+      end
+      
+      if user.notification_settings.textMessages == "on"
+        sendTextMessage(user.notification_settings.phoneNumber, "The status of your maintenance request that was submitted on " + 
+                                                                maintenanceRequestDate.strftime("%m/%d/%y") + " by " + userFirstName + " " +
+                                                                userLastName + " has changed to " + status + "."
+                       )
+      end
+      
+    end
+  end
+
+  def sendTextMessage(numberToSendTo, textMessageBody)
+    twilio_sid = "AC44d7376dc48f735adf59d7a242f87963"
+    twilio_token = "30cb912b711e15d31ec41aa2f7d2fcc7"
+    twilio_phone_number = "2094424289"
+
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+    @twilio_client.account.sms.messages.create(
+      :from => "+1#{twilio_phone_number}",
+      :to => numberToSendTo,
+      :body => textMessageBody
+    )
   end
 
 end
